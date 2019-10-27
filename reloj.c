7648a651a8f63402f8f99c56b964263a95c074cf
Rodrigo Alvarez <rodrigoalvarez449@gmail.com>
2019-10-27 14:00:13 -0600

    Se ha agregado el codigo del reloj

--- reloj.c
+++ reloj.c
@@ -1,25 +1,53 @@
 #include <stdio.h>
-#include <Windows.h>
-
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
 int main(){
-    int doLoop = 0, cont = 0;
-    int ss[1], mm[1], hh[1];
+    bool isLooping = true;
+    int h_1, h_2, m_1, m_2, s_1, s_2, h_Usr, m_Usr, s_Usr;
+    //h_1-h_2 : m_1-m_2 : s_1-s_2
     printf("Introduzca la hora de inicio: ");
-    scanf("%d", hh);
+    scanf("%d", &h_1);
     printf("Introduzca los minutos de inicio: ");
-    scanf("%d", mm);
+    scanf("%d", &m_1);
     printf("Introduzca los segundos de inicio: ");
-    scanf("%d", ss);
-    printf("%d %d : %d %d : %d %d", hh[0], hh[1], mm[0], mm[1], ss[0], ss[1]);
-    
-    /*do {
-        system("clear");
-        Sleep(1000);
-        cont++;
-        printf("%d\n", cont);
-    }while(doLoop!=1);
-    */
+    scanf("%d", &s_1);
+
+    printf("Introduzca la hora de termino: ");
+    scanf("%d", &h_Usr);
+    printf("Introduzca los minutos de termino: ");
+    scanf("%d", &m_Usr);
+    printf("Introduzca los segundos de termino: ");
+    scanf("%d", &s_Usr);
+
+    h_2 = h_1 % 10; h_1 -= h_2; m_2 = m_1 % 10; m_1 /= 10; s_2 = s_1 % 10; s_1 /= 10;
+
+    while(isLooping){
+        int hAct = (h_1*10)+h_2;
+        int mAct = (m_1*10)+m_2;
+        int sAct = (s_1*10)+s_2;
+        printf("%d%d : %d%d : %d%d\n", h_1,h_2,m_1,m_2,s_1,s_2);
+        sleep(1);
+        s_2++;
+        if(s_2>9){
+            s_1++;
+            s_2=0;
+        }
+        if(s_1>5){
+            m_2++;
+            s_1=0;
+        }
+        if(m_2>9){
+            m_1++;
+            m_2=0;
+        }
+        if(m_1>5){
+            h_2++;
+            m_1=0;
+        }
+        if(hAct == h_Usr && mAct == m_Usr && sAct >= s_Usr && sAct <= (s_Usr+10)){
+            printf("\nLa alarma ha sonado\a\n");
+        }
+    }
     return 0;
 }
-
-//PENDIENTE
